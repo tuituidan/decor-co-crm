1e4dcfc2c1cea2e16424886e69ce215f12afcd58
zhujunhan <zhujunhan@thunisoft.com>
2020-11-02 15:39:52 +0800

    init

---
+++ src/main/java/com/tuituidan/crm/DecorCoCrmApplication.java
@@ -0,0 +1,13 @@
+package com.tuituidan.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecorCoCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecorCoCrmApplication.class, args);
+    }
+
+}
