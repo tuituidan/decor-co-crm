79cc113c2d7e309b7a6ecee4622202220c80b640
zhujunhan <136375002@qq.com>
2020-11-06 20:41:52 +0800

    升级Springboot

--- src/main/java/com/tuituidan/crm/DecorCoCrmApplication.java
+++ src/main/java/com/tuituidan/crm/DecorCoCrmApplication.java
@@ -3,6 +3,11 @@ package com.tuituidan.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * DecorCoCrmApplication.
+ *
+ * @author tuituidan
+ */
 @SpringBootApplication
 public class DecorCoCrmApplication {
 
